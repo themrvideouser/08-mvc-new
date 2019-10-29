8f6b50281eea4be9ce2467f6ac03c41ca6a11dc3
themrvideouser <frederik.gschaider@gmail.com>
2019-10-29 15:38:12 +0100

    Commit # 5 Formular

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")\
+    welcometext = "Herzlich Willkommen!"
+    currentyear = datetime.datetime.now().year
+    vemap = ["Martin", "Felix", "Anna", "Markus", "Yevgen"]
+    rich = True
+    return render_template("index.html", welcometext=welcometext, currentyear=currentyear, vemap=vemap, rich=rich)
 
 
-
-@app.route("/about-me")
+@app.route("/about-me", methods=["GET"])
 def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST"])
 def contact():
-    return render_template("contact.html")
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+    return render_template("success.html")
 
 @app.route("/kaboom")
 def kaboom():
