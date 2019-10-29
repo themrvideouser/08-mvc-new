e1861cb57ea7762819b291ab8b5d6cef89fc8f91
themrvideouser <frederik.gschaider@gmail.com>
2019-10-29 16:28:45 +0100

    Commit # 6 Formular + Cookie

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import datetime
 
 app = Flask(__name__)
@@ -13,20 +13,21 @@ def index():
     return render_template("index.html", welcometext=welcometext, currentyear=currentyear, vemap=vemap, rich=rich)
 
 
-@app.route("/about-me", methods=["GET"])
+@app.route("/about-me", methods=["GET", "POST"])
 def about():
-    return render_template("about.html")
-
-
-@app.route("/contact", methods=["POST"])
-def contact():
-    contact_name = request.form.get("contact-name")
-    contact_email = request.form.get("contact-email")
-    contact_message = request.form.get("contact-message")
-    print(contact_name)
-    print(contact_email)
-    print(contact_message)
-    return render_template("success.html")
+    if request.method == "GET":
+        user_name = request.cookies.get("user_name")
+        return render_template("about.html", name=user_name)
+    elif request.method == "POST":
+        contact_name = request.form.get("contact-name")
+        contact_email = request.form.get("contact-email")
+        contact_message = request.form.get("contact-message")
+        print(contact_name)
+        print(contact_email)
+        print(contact_message)
+        response = make_response(render_template("success.html"))
+        response.set_cookie("user_name", contact_name)
+        return response
 
 @app.route("/kaboom")
 def kaboom():
